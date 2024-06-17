a72d9655bd02eae992310e655e426813d4e79b74
Andrei Belskii <mrandrewer@yandex.ru>
2024-06-17 23:27:10 +0300

    Start locale

---
+++ locales/en.js
@@ -0,0 +1,14 @@
+export default {
+    translation: {
+        heading: 'RSS reader',
+        lead: 'Start reading RSS today! It`s easy, it`s beautiful.',
+        urlPlaceholder: 'RSS link',
+        urlExample: 'Example: https://lorem-rss.hexlet.app/feed',
+        addBtnText: 'Add',
+        errors: {
+            required: 'This field is required',
+            invalid: 'Link must be a correct URL',
+            alreadyExists: 'RSS feed is already in the list',
+        },
+    }
+};
--- src/validation.js
+++ src/validation.js
@@ -1,22 +1,12 @@
 import * as yup from 'yup';
 import _ from 'lodash';
 
-const schema = yup.object().shape({
-  feed: yup.string().url().required(),
-});
-
 const validate = (state) => {
   const { form } = state;
+  const schema = yup.object().shape({
+    feed: yup.string().url().required().notOneOf(state.feeds),
+  });
   schema.validate(state.form.fields, { abortEarly: false })
-    .then(() => {
-      if (state.feeds.includes(state.form.fields.feed)) {
-        form.errors = { feed: { message: 'This feed is already added' } };
-        form.state = 'invalid';
-      } else {
-        form.errors = { };
-        form.state = 'valid';
-      }
-    })
     .catch((e) => {
       form.errors = _.keyBy(e.inner, 'path');
       form.state = 'invalid';
